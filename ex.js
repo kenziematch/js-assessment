4f7c8adf552457b3f5d30018a6deac06af517808
kenziematch <ayelabukola08@gmail.com>
2021-05-28 20:55:28 -0230

    logging on console

--- ex.js
+++ ex.js
@@ -1,1 +1,23 @@
-alert("welcome to my first project"); 
+let myName = 'Ayela Bukola';
+myName ='abigail';
+console.log(myName);
+
+let dateOfBirth = 08-03-1996;
+dateOfBirth = 03;
+console.log(dateOfBirth);
+
+let myButton = document.querySelector('button');
+
+
+function setUserColor() {
+    let color = prompt('What is your favorite color?');
+    console.log('Your favorite color is ' + color);
+    
+
+}
+myButton.onclick = function() {
+    setUserColor();
+  }
+
+  let myPotato = `ore said i'm a "queen", what does that mean?`;
+  console.log(myPotato);
